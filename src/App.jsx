de45f6ed4460b727ba7d1f34be07dbada5a0942f
Suraj P <khayapiya101@gmail.com>
2022-08-21 22:57:02 +0530

    Workspace ready for project development 	- Installed MUI packages 	- Cleared react built in comments from public/index.html 	- Walkthough of material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
+// MUI
+import { Typography, styled, Button } from "@mui/material";
+
 const App = () => {
-  return <div>App</div>;
+  const BlueButton = styled(Button)(({ theme }) => ({
+    color: "white",
+    backgroundColor: "blue",
+    margin: 4,
+    "&:hover": {
+      backgroundColor: "darkblue",
+    },
+  }));
+
+  const SecondaryBtn = styled(Button)(({ theme }) => ({
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.secondary.main,
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.light,
+    },
+  }));
+
+  return (
+    <>
+      <Typography
+        variant="h3"
+        gutterBottom
+        component={"p"}
+        sx={{
+          color: "purple",
+        }}
+      >
+        It uses h3 variant but it's a p tag
+      </Typography>
+      <Typography variant="h6" color={"primary"}>
+        this is h6 tag
+      </Typography>
+      <BlueButton>My button</BlueButton>
+      <SecondaryBtn>Secondary Btn</SecondaryBtn>
+    </>
+  );
 };
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "rgb(200,0,0)",
+      light: "rgb(100,0,0)",
+      dark: "rgb(255,0,0)",
+    },
+    secondary: {
+      main: "#888",
+      light: "#999",
+    },
+  },
+});
